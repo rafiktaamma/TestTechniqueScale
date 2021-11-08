23be9b1f82cf9fc01df84306ee8fa321a938d1be
rafik <b.taamma@esi-sba.dz>
2021-11-08 21:06:37 +0100

    projet setup

---
+++ src/main/java/com/rafiktaamma/TestTechniqueScale/TestTechniqueScaleApplication.java
@@ -0,0 +1,13 @@
+package com.rafiktaamma.TestTechniqueScale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTechniqueScaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTechniqueScaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafiktaamma/TestTechniqueScale/TestTechniqueScaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafiktaamma.TestTechniqueScale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTechniqueScaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
